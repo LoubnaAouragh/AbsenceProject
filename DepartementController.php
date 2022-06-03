537806e1ad9d8e77dc4185a090faf661979191bd
MOUAD BEN MEKKI <73074210+BenMekkiMouad@users.noreply.github.com>
2022-06-03 16:12:02 +0100

    departement get nom in vue by index function

---
+++ DepartementController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Departement;
+use App\Http\Requests\StoreDepartementRequest;
+use App\Http\Requests\UpdateDepartementRequest;
+
+class DepartementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $departement = new Departement;
+        $departement =  $departement::all();
+        return view('departement.index', compact('departement'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreDepartementRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDepartementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Departement  $departement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Departement $departement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Departement  $departement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Departement $departement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateDepartementRequest  $request
+     * @param  \App\Models\Departement  $departement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDepartementRequest $request, Departement $departement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Departement  $departement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Departement $departement)
+    {
+        //
+    }
+}
